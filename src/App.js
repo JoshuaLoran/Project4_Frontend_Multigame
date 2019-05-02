c8f916c58ce9f68107e0fe3ab0f2d0be71a5bc72
Joshua Loran <joshua.loran@gmail.com>
2019-05-02 15:34:25 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import ActionCable from 'actioncable'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    array: []
+  }
+
+  componentDidMount() {
+    window.fetch('http://localhost:3001/games/1')
+      .then(res => res.json())
+      .then(json => console.log(json))
+
+
+      // .then(json => this.setState({
+      //   array: json.array
+      // }))
+
+
+/////   UNMODIFIED BELOW      /////////
+    const cable = ActionCable.createConsumer('ws://localhost:3001/cable')
+    this.sub = cable.subscriptions.create('GamesChannel', {
+      received: this.handleReceiveNewText
+    })
+  }
+
+  handleReceiveNewData = (data) => {
+    console.log(data)
+    // if (data !== this.state.array) {
+    //   this.setState({ text })
+    // }
+  }
+
+  handleChange = e => {
+    let newArr = e.target.value
+    console.log(e.target.value)
+    this.setState({ array: newArr })
+    this.sub.send({ array: newArr, id: 1 })
+  }
+
+  render() {
+    return (
+      <textarea
+        value={JSON.stringify(this.state.array)}
+        onChange={this.handleChange}
+      />
+    )
+  }
 }
 
-export default App;
+export default App
